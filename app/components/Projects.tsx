aa04a6a76101e70076372971fdf8817afbbc0b70
mohamedzouine827 <mohamedzouine82o@gmail.com>
2024-03-20 04:37:53 +0000

    8

--- app/components/Projects.tsx
+++ app/components/Projects.tsx
@@ -1,7 +1,7 @@
 
 import Image from "next/image";
 import ProjectTitle from "./ProjectTitle";
-import Images from "@/app/assets/image_2024-03-13_233420172.jpg";
+import Images from "@/app/assets/aa.png";
 import Link from "next/link";
 ;
 
@@ -15,7 +15,7 @@ export default function Projects() {
         <ProjectTitle text="Projects🔨" />
       </div>
       <div className="mt-12 ml-4 md:ml-12">
-        <div className="border-2 border-white h-[600px] w-[80vw] md:w-[56vw] md:h-[300px] flex flex-col md:flex-row items-center justify-center md:justify-start ">
+        <div className="border-b-2 border-l-2 border-t-[0.001vh] border-r-[0.001vh] border-white h-[50vh] md:h-[40vh] w-[80vw] md:w-[52vw]  flex flex-col md:flex-row items-center justify-center md:justify-start ">
           <div>
           <Link href="https://flavorsfusion.vercel.app">
             <Image
@@ -28,16 +28,16 @@ export default function Projects() {
             
           </Link>
           <div  className=" hidden md:block md:absolute md:ml-10 md:text-xl md:font-bold md:mt-[-48px] md:text-white md:opacity-100 md:transition md:duration-400 md:ease-in-out md:hover:opacity-50 ">
-            Flavours Fusion
+            Zn Agency
           </div>
           </div>
-          <div className=" ml-12 flex-row md:flex-col  ">
-            <div className="justify-center items-center md:mt-0 mt-4 mr-8 md:mr-0 flex md:justify-start text-xl md:text-3xl font-bold text-white opacity-100 transition duration-400 ease-in-out hover:opacity-50 ">
-                Flavours Fusion
+          <div className=" md:ml-12 flex-row md:flex-col  ">
+            <div className="justify-center items-center md:mt-0 mt-4 mr-8 md:mr-0 flex md:justify-center text-xl md:text-3xl font-bold text-white opacity-100 transition duration-400 ease-in-out hover:opacity-50 ">
+                Zn Agency
             </div>
             <br/>
-            <div className=" mt-1 text-white opacity-100 text-light flex justify-center items-center md:justify-start md:items-start transition duration-400 ease-in-out hover:opacity-50">
-            This website lacks content and responsiveness due to the absence <br/> of a client. To prevent potential theft, I&apos;ve refrained from implementing<br/> responsiveness. However, I recognize the need to improve <br/> both aspects for a better user experience in the future
+            <div className=" p-[2vw] mt-1 text-white opacity-100 text-light flex justify-center items-center text-center md:justify-center md:text-center md:items-start transition duration-400 ease-in-out hover:opacity-50">
+            Zn Agency is a Costume Website For an Agency X
             </div>
           </div>
         </div>
